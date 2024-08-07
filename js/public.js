1ebbdff4e88851f634f684811b916f2e32b14d5a
SaSaChu <teresa7397@gmail.com>
2024-08-07 12:58:29 +0800

    開版

---
+++ js/public.js
@@ -0,0 +1,11 @@
+
+$(function() {
+	
+	var myModal = document.getElementById('myModal')
+	var myInput = document.getElementById('myInput')
+
+	myModal.addEventListener('shown.bs.modal', function () {
+	  myInput.focus()
+	});
+
+});
